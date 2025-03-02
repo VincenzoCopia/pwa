3f016b6cd0f94f23613278dee86852b3ba110c18
Vincenzo Copia <vincenzo.copia@gmail.com>
2025-03-02 09:32:29 +0100

    NFC Update script.js

--- static/script.js
+++ static/script.js
@@ -31,3 +31,22 @@ updateNetworkStatus();
 // Ascoltatori per gli eventi online e offline
 window.addEventListener('online', updateNetworkStatus);
 window.addEventListener('offline', updateNetworkStatus);
+
+
+// Lettura NFC
+const nfcMessageElement = document.getElementById('nfcMessage');
+
+if ("NDEFReader" in window) {
+    const reader = new NDEFReader();
+    reader.scan().then(() => {
+        nfcMessageElement.textContent = "Scanner NFC attivo, avvicina un tag";
+        reader.onreading = (event) => {
+            const { serialNumber } = event;
+            nfcMessageElement.textContent = `Tag NFC rilevato! UID: ${serialNumber}`;
+        };
+    }).catch((error) => {
+        nfcMessageElement.textContent = `Errore scanner NFC: ${error}`;
+    });
+} else {
+    nfcMessageElement.textContent = "Web NFC non supportato in questo browser";
+}
